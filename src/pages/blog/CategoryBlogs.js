bf38dd9751f8f1df5febf7b0e0f970b49d4f21d1
parvesh <vparvesh@gmail.com>
2023-06-30 18:22:44 +0530

    set up standard background of application

--- src/pages/blog/CategoryBlogs.js
+++ src/pages/blog/CategoryBlogs.js
@@ -2,16 +2,20 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import BlogCards from "../../components/blog/BlogCards";
 import ErrorBoundary from '../../components/errorBoundary/ErrorBoundary'
+import styles from './CategoryBlog.module.scss'
 
 export default function CategoryBlogs() {
   const { blogCategory } = useParams();
   const title = blogCategory.toUpperCase().replace(/-/g, " ").trim();
+  
   return (
-    <div className="my-3 container" style={{ minHeight: "80vh" }}>
-      <h2 className="text-primary fs-4 fs-md-2 mb-2 mb-md-4">{title}</h2>
+    <div className={` ${styles.categoryBlogPage}`}>
+      <h2 className="text-light fs-4 fs-md-2 mb-2 mb-md-4">{title}</h2>
       <ErrorBoundary>
         <BlogCards category={blogCategory} />
       </ErrorBoundary>
     </div>
   );
 }
+
+
