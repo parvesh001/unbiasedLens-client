b2ef0fd88ae94b45c3c5bfb7eeac33b0d49d0e38
parvesh <vparvesh@gmail.com>
2023-07-08 14:13:03 +0530

    protected author specific route of uploading profile

--- src/pages/profile/UploadProfilePage.js
+++ src/pages/profile/UploadProfilePage.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import UploadProfile from "../../components/profile/UploadProfile";
+import { AuthContext } from "../../context/authContext";
+import { useParams } from "react-router-dom";
 
 export default function UploadProfilePage() {
-  return <UploadProfile />;
+  const { id } = useParams();
+  const { author } = useContext(AuthContext);
+
+  const content =
+    author && author._id === id ? (
+      <UploadProfile />
+    ) : (
+      <p className="text-light text-center fs-4 my-5" style={{minHeight:'86vh'}}>This route is author specific route, please go back</p>
+    );
+
+  return content;
 }
